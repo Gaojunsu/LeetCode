6ff829fb53450d8737896ac1ec78ec1a897d99c7
jean <jean_su@hanbo.com>
2019-12-10 17:46:22 +0800

    LeetCode 算题

--- app/src/main/java/com/LeetCode/study/MainActivity.java
+++ app/src/main/java/com/LeetCode/study/MainActivity.java
@@ -3,14 +3,13 @@ package com.LeetCode.study;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
+
+import com.LeetCode.study.dataStructure.ListNode;
 
 public class MainActivity extends AppCompatActivity {
 
-    // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("native-lib");
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,12 +18,10 @@ public class MainActivity extends AppCompatActivity {
 
         // Example of a call to a native method
         TextView tv = findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+
+        new LengthOfLongestSubstring().test();
+
     }
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
+
 }
